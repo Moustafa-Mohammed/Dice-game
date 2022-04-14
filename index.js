73c094ad65a1f1cf988166c38d69b09d49be39e5
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2022-04-14 03:47:46 +0200

    Downloaded the skeleton html and css code,added a play button to refresh the page using location.reload() function,  changed some styles, and added the logic using vanilla JS

---
+++ index.js
@@ -0,0 +1,22 @@
+const imageOne = document.querySelector(".img1");
+const imageTwo = document.querySelector(".img2");
+const playBtn = document.querySelector(".btn");
+
+const playerOneDice = Math.floor(Math.random() * 6) + 1;
+
+const playerTwoDice = Math.floor(Math.random() * 6) + 1;
+
+imageOne.setAttribute("src", "./images/dice" + playerOneDice + ".png");
+imageTwo.setAttribute("src", "./images/dice" + playerTwoDice + ".png");
+
+const refresh = () => {
+  location.reload();
+};
+
+playBtn.addEventListener("click", refresh);
+
+if (playerOneDice > playerTwoDice) {
+  document.querySelector("h1").textContent = "🚩Player 1 wins!";
+} else if (playerTwoDice > playerOneDice) {
+  document.querySelector("h1").textContent = "Player 2 wins! 🚩";
+}
